a8d0e25a0a1acc091edea47eeac0ed78c83c4d1c
Rick Love <rick@toldpro.com>
2021-01-05 22:47:08 -0600

    Remove .js from git

--- bdsx-node-servers/love-family-server/src/mathGame.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWF0aEdhbWUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJtYXRoR2FtZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
